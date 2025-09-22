21bc574e29f7f7829aa46a2267e16145c8353f65
David Lönnhager <david.l@mullvad.net>
2025-09-22 16:54:24 +0200

    fixup

--- boringtun/src/device/mod.rs
+++ boringtun/src/device/mod.rs
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 Cloudflare, Inc. All rights reserved.
+// Copyright (c) 2019 Cloudflare, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
 pub mod allowed_ips;
