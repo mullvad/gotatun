7e92ddc3c8760e047bd8d6f69cc4f771c9382888
David Lönnhager <david.l@mullvad.net>
2025-09-26 16:48:47 +0200

    Implement and disable UDP GRO on Windows

--- boringtun/src/udp/socket/windows.rs
+++ boringtun/src/udp/socket/windows.rs
@@ -1,4 +1,4 @@
-use std::{io, net::SocketAddr};
+use std::{ffi::c_char, io, net::SocketAddr};
 use tokio::io::Interest;
 
 use crate::{
@@ -71,7 +71,6 @@ impl UdpSend for super::UdpSocket {
                 // If destination differs, stop coalescing
                 if next_addr != dest {
                     saved_packet = Some((next_packet, next_addr));
-                    // TODO: make sure we do not lose this
                     break;
                 }
 
@@ -128,8 +127,35 @@ impl UdpSend for super::UdpSocket {
     fn local_addr(&self) -> io::Result<Option<SocketAddr>> {
         UdpSocket::local_addr(self).map(Some)
     }
+
+    #[cfg(feature = "windows-gro")]
+    /// Enable receive offloading
+    fn enable_udp_gro(&self) -> io::Result<()> {
+        let raw_sock = self.inner.as_raw_socket();
+        // Same as msquic
+        let val = u32::from(u16::MAX);
+
+        // SAFETY: We are passing valid pointers
+        let result = unsafe {
+            libc::setsockopt(
+                usize::try_from(raw_sock).unwrap(),
+                WinSock::IPPROTO_UDP,
+                WinSock::UDP_RECV_MAX_COALESCED_SIZE,
+                (&val) as *const u32 as *const c_char,
+                mem::size_of_val(&val) as i32,
+            )
+        };
+
+        if result == 0 {
+            log::debug!("Enabled UDP GRO");
+            Ok(())
+        } else {
+            Err(io::Error::last_os_error())
+        }
+    }
 }
 
+#[cfg(not(feature = "windows-gro"))]
 impl UdpRecv for super::UdpSocket {
     type RecvManyBuf = ();
 
@@ -138,6 +164,228 @@ impl UdpRecv for super::UdpSocket {
         let (n, src) = self.inner.recv_from(&mut buf).await?;
         buf.truncate(n);
         Ok((buf, src))
+    }
+}
+
+#[cfg(feature = "windows-gro")]
+mod gro {
+    use super::*;
+
+    use std::ptr;
+
+    use socket2::{Domain, SockAddr, SockAddrStorage, Type};
+    use std::{mem, os::windows::io::AsRawSocket, sync::LazyLock};
+    use windows_sys::Win32::Networking::WinSock::{self, SOCKADDR_INET, WSABUF, WSAMSG};
+
+    const MAX_RECV_PACKETS: usize = 100;
+
+    pub struct RecvManyBuf {
+        // TODO: create a single packet buf and split it?
+        gro_buf: Vec<u8>,
+        cmsg: Cmsg,
+    }
+
+    impl Default for RecvManyBuf {
+        fn default() -> Self {
+            let cmsg = Cmsg::new(
+                mem::size_of::<u32>(),
+                WinSock::IPPROTO_UDP,
+                WinSock::UDP_COALESCED_INFO as i32,
+            );
+            Self {
+                gro_buf: vec![],
+                cmsg,
+            }
+        }
+    }
+
+    impl UdpRecv for super::UdpSocket {
+        type RecvManyBuf = RecvManyBuf;
+
+        async fn recv_from(
+            &mut self,
+            pool: &mut PacketBufPool,
+        ) -> io::Result<(Packet, SocketAddr)> {
+            let mut buf = pool.get();
+            let (n, src) = self.inner.recv_from(&mut buf).await?;
+            buf.truncate(n);
+            Ok((buf, src))
+        }
+
+        async fn recv_many_from(
+            &mut self,
+            recv_buf: &mut Self::RecvManyBuf,
+            pool: &mut PacketBufPool,
+            packets: &mut Vec<(Packet, SocketAddr)>,
+        ) -> io::Result<()> {
+            let socket = self.inner.clone();
+            recv_buf.gro_buf.resize(MAX_RECV_PACKETS * 1500, 0);
+
+            let msg = self
+                .inner
+                .async_io(Interest::READABLE, || {
+                    recvmsg(recv_buf.gro_buf.as_mut_slice(), &mut recv_buf.cmsg, &socket)
+                })
+                .await?;
+
+            recv_buf
+                .gro_buf
+                .truncate(usize::try_from(msg.bytes_received).unwrap());
+
+            if msg.gro_size == 0 {
+                // Single packet
+                let mut buf = pool.get();
+                buf.buf_mut().clear();
+                buf.buf_mut().extend_from_slice(&recv_buf.gro_buf);
+                packets.push((buf, msg.source_addr));
+                return Ok(());
+            }
+
+            // Split into multiple packets
+            // TODO: Consider reading into one big buffer and splitting it
+            for segment in recv_buf
+                .gro_buf
+                .chunks(usize::try_from(msg.gro_size).unwrap())
+            {
+                let mut buf = pool.get();
+                buf.buf_mut().clear();
+                buf.buf_mut().extend_from_slice(segment);
+                packets.push((buf, msg.source_addr));
+            }
+
+            Ok(())
+        }
+
+        fn max_number_of_packets_to_recv(&self) -> usize {
+            MAX_RECV_PACKETS
+        }
+    }
+
+    struct RecvMsg {
+        bytes_received: u32,
+        gro_size: u32,
+        source_addr: SocketAddr,
+    }
+
+    /// Receive GRO segments into `buffer` using `WSARecvMsg`
+    fn recvmsg(
+        buffer: &mut [u8],
+        cmsg: &mut Cmsg,
+        socket: &tokio::net::UdpSocket,
+    ) -> io::Result<RecvMsg> {
+        use windows_sys::Win32::Networking::WinSock::LPFN_WSARECVMSG;
+
+        const UDP_COALESCED_INFO: i32 = WinSock::UDP_COALESCED_INFO as i32;
+
+        // Load WSARecvMsg
+        static RECVMSG: LazyLock<LPFN_WSARECVMSG> = LazyLock::new(|| {
+            let mut bytes_returned: u32 = 0;
+            let mut func: LPFN_WSARECVMSG = None;
+
+            let sock = socket2::Socket::new(Domain::IPV4, Type::DGRAM, None)
+                .inspect_err(|err| {
+                    log::error!("Failed to create socket: {err}");
+                })
+                .ok()?;
+
+            let guid = WinSock::WSAID_WSARECVMSG;
+            let result = unsafe {
+                WinSock::WSAIoctl(
+                    sock.as_raw_socket() as usize,
+                    WinSock::SIO_GET_EXTENSION_FUNCTION_POINTER,
+                    &guid as *const _ as *mut _,
+                    mem::size_of_val(&guid) as u32,
+                    &mut func as *mut _ as *mut _,
+                    mem::size_of_val(&func) as u32,
+                    &mut bytes_returned as *mut _,
+                    ptr::null_mut(),
+                    None,
+                )
+            };
+
+            if result != 0 {
+                log::error!(
+                    "Failed to get WSARecvMsg function pointer: {}",
+                    io::Error::last_os_error()
+                );
+                None
+            } else {
+                func
+            }
+        });
+
+        if buffer.len() == 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "buffer must be non-empty",
+            ));
+        }
+
+        let recvmsg = RECVMSG.expect("missing WSARecvMsg");
+
+        let ctrl = WSABUF {
+            len: cmsg.buffer.len() as u32,
+            buf: cmsg.buffer.as_mut_ptr(),
+        };
+
+        let mut source = SOCKADDR_INET::default();
+
+        let mut data = WSABUF {
+            len: buffer.len() as u32,
+            buf: buffer.as_mut_ptr() as *mut u8,
+        };
+
+        let mut msg = WSAMSG {
+            name: &mut source as *mut _ as *mut _,
+            namelen: mem::size_of_val(&source) as i32,
+            lpBuffers: &mut data,
+            dwBufferCount: 1,
+            Control: ctrl,
+            dwFlags: 0,
+        };
+
+        let mut len = 0;
+        // SAFETY: All pointers are valid and point to initialized data
+        // The lengths are correct
+        let status = unsafe {
+            (recvmsg)(
+                socket.as_raw_socket() as usize,
+                &mut msg,
+                &mut len,
+                ptr::null_mut(),
+                None,
+            )
+        };
+        if status == -1 {
+            return Err(io::Error::last_os_error());
+        }
+
+        let mut gro_size = 0;
+
+        if cmsg.header().cmsg_type == UDP_COALESCED_INFO {
+            // SAFETY: We have allocated space for a u32 in the CMSG
+            gro_size = unsafe { *(cmsg.data_mut_ptr() as *const u32) };
+        }
+
+        let source = try_socketaddr_from_inet_sockaddr(source)
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "invalid source address"))?;
+
+        Ok(RecvMsg {
+            bytes_received: len,
+            gro_size,
+            source_addr: source,
+        })
+    }
+
+    /// Converts a `SOCKADDR_INET` to `SocketAddr`. Returns `None` if the family is not valid.
+    pub fn try_socketaddr_from_inet_sockaddr(addr: SOCKADDR_INET) -> Option<SocketAddr> {
+        // SAFETY: SOCKADDR_INET and SockAddrStorage have the same layout
+        unsafe {
+            let mut storage: SockAddrStorage = mem::zeroed();
+            *(&mut storage as *mut _ as *mut SOCKADDR_INET) = addr;
+            SockAddr::new(storage, mem::size_of_val(&addr) as i32)
+        }
+        .as_socket()
     }
 }
 
@@ -172,6 +420,14 @@ impl Cmsg {
         debug_assert!(hdr.is_aligned());
         // SAFETY: `hdr` is aligned and points to an initialized `CMSGHDR`
         unsafe { &mut *hdr }
+    }
+
+    #[cfg(feature = "windows-gro")]
+    fn header(&mut self) -> &CMSGHDR {
+        let hdr = self.buffer.as_mut_ptr() as *const CMSGHDR;
+        debug_assert!(hdr.is_aligned());
+        // SAFETY: `hdr` is aligned and points to an initialized `CMSGHDR`
+        unsafe { &*hdr }
     }
 
     pub fn as_slice(&self) -> &[u8] {
