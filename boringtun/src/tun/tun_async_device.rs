afc469b60f5beff75cd2842a9843bf9678725817
Joakim Hulthe <joakim.hulthe@mullvad.net>
2025-10-03 14:28:03 +0200

    fixup: Check packet_information

--- boringtun/src/tun/tun_async_device.rs
+++ boringtun/src/tun/tun_async_device.rs
@@ -30,6 +30,10 @@ struct TunDeviceState {
 impl TunDevice {
     /// Construct from a [tun::AsyncDevice].
     pub fn from_tun_device(tun: tun::AsyncDevice) -> io::Result<Self> {
+        if tun.packet_information() {
+            return Err(io::Error::other("packet_information is not supported"));
+        };
+
         let mtu = tun.mtu()?;
         let (tx, rx) = watch::channel(mtu);
 
