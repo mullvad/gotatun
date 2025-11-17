65a61ccac9dae8c86553d32c7a0763b9ef698706
Markus Pettersson <markus.pettersson@mullvad.net>
2025-11-17 17:00:18 +0100

    Add nix flake for building `gotatun-cli`.
    Includes a devshell with all dependencies as well.

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "GotaTun: A userspace implemenation of WireGuard written in Rust.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      # Support all Linux systems that the nixpkgs flake exposes
+      inherit (nixpkgs) lib;
+      systems = lib.intersectLists lib.systems.flakeExposed lib.platforms.linux;
+      forAllSystems = lib.genAttrs systems;
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          rust-bin = rust-overlay.lib.mkRustBin { } pkgs;
+          rust-toolchain = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ rust-toolchain ];
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          rust-bin = rust-overlay.lib.mkRustBin { } pkgs;
+          rust-toolchain = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          default = self.packages.${system}.gotatun;
+          gotatun = pkgs.rustPlatform.buildRustPackage {
+            pname = "gotatun";
+            version = self.shortRev or self.dirtyShortRev or "unknown";
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./gotatun
+                ./gotatun-cli
+                ./Cargo.toml
+                ./Cargo.lock
+              ];
+            };
+            cargoLock.lockFile = ./Cargo.lock;
+            strictDeps = true;
+            buildNoDefaultFeatures = true;
+            nativeBuildInputs = [
+              rust-toolchain
+            ];
+            meta = {
+              mainProgram = "gotatun";
+              description = "Userspace WireGuard";
+              homepage = "https://github.com/mullvad/gotatun";
+              license = lib.licenses.bsd3;
+              platforms = lib.platforms.linux;
+            };
+          };
+        }
+      );
+    };
+}
