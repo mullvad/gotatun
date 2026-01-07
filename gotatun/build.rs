7cf0f96cb061faf26950e73d3b1f8c863297e5df
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-01-07 11:26:04 +0100

    fixup! Fail CI on cargo doc warnings

--- gotatun/build.rs
+++
@@ -1,7 +0,0 @@
-pub fn main() {
-    // Enable the use of `#[cfg(profile = "release")]`-attributes.
-    println!(r#"cargo:rustc-check-cfg=cfg(profile, values("release"))"#);
-    if let Ok(profile) = std::env::var("PROFILE") {
-        println!("cargo:rustc-cfg=profile={:?}", profile);
-    }
-}
