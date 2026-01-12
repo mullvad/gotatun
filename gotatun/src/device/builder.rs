ee26f948bc9a335bed03175e6d2e91b957d4ab08
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-01-12 17:00:27 +0100

    Add peer constructors to DeviceBuilder

--- gotatun/src/device/builder.rs
+++ gotatun/src/device/builder.rs
@@ -8,11 +8,15 @@ use tokio::sync::{Mutex, RwLock};
 #[cfg(feature = "tun")]
 use crate::device::Error;
 use crate::{
-    device::{Device, DeviceState, allowed_ips::AllowedIps, api::ApiServer},
+    device::{
+        Device, DeviceState, allowed_ips::AllowedIps, api::ApiServer, peer::builder::PeerBuilder,
+    },
     task::Task,
     tun::{IpRecv, IpSend, tun_async_device::TunDevice},
     udp::{UdpTransportFactory, socket::UdpSocketFactory},
 };
+
+use super::Connection;
 
 pub struct Nul;
 
@@ -21,15 +25,19 @@ pub struct DeviceBuilder<Udp, TunTx, TunRx> {
     tun_tx: TunTx,
     tun_rx: TunRx,
     uapi: Option<ApiServer>,
+
+    // TODO: consider turning this into a typestate, and adding a special case for single peer
+    peers: Vec<PeerBuilder>,
 }
 
 impl DeviceBuilder<Nul, Nul, Nul> {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
             udp: Nul,
             tun_tx: Nul,
             tun_rx: Nul,
             uapi: None,
+            peers: Vec::new(),
         }
     }
 }
@@ -45,6 +53,7 @@ impl<X, Y> DeviceBuilder<Nul, X, Y> {
             tun_tx: self.tun_tx,
             tun_rx: self.tun_rx,
             uapi: self.uapi,
+            peers: self.peers,
         }
     }
 }
@@ -81,6 +90,7 @@ impl<X> DeviceBuilder<X, Nul, Nul> {
             tun_tx,
             tun_rx,
             uapi: self.uapi,
+            peers: self.peers,
         }
     }
 }
@@ -90,11 +100,16 @@ impl<X, Y, Z> DeviceBuilder<X, Y, Z> {
         self.uapi = Some(uapi);
         self
     }
+
+    pub fn with_peer(mut self, peer: PeerBuilder) -> Self {
+        self.peers.push(peer);
+        self
+    }
 }
 
 impl<Udp: UdpTransportFactory, TunTx: IpSend, TunRx: IpRecv> DeviceBuilder<Udp, TunTx, TunRx> {
-    pub fn build(self) -> Device<(Udp, TunTx, TunRx)> {
-        let state = DeviceState {
+    pub async fn build(self) -> Result<Device<(Udp, TunTx, TunRx)>, Error> {
+        let mut state = DeviceState {
             api: None,
             udp_factory: self.udp,
             tun_tx: Arc::new(Mutex::new(self.tun_tx)),
@@ -111,6 +126,12 @@ impl<Udp: UdpTransportFactory, TunTx: IpSend, TunRx: IpRecv> DeviceBuilder<Udp, 
             connection: None,
         };
 
+        let has_peers = !self.peers.is_empty();
+        for peer in self.peers {
+            let index = state.next_index();
+            state.add_peer(peer, index);
+        }
+
         let inner = Arc::new(RwLock::new(state));
 
         if let Some(uapi) = self.uapi {
@@ -120,6 +141,12 @@ impl<Udp: UdpTransportFactory, TunTx: IpSend, TunRx: IpRecv> DeviceBuilder<Udp, 
             ))
         }
 
-        Device { inner }
+        if has_peers {
+            let con = Connection::set_up(inner.clone()).await?;
+            let mut state = inner.write().await;
+            state.connection = Some(con);
+        }
+
+        Ok(Device { inner })
     }
 }
