e253b8ec4b0b7b1ec794b4357e829264382ff852
Markus Pettersson <markus.pettersson@mullvad.net>
2026-02-20 17:31:54 +0100

    fixup! Stop using ring as an entropy source in benchmarks and tests

--- gotatun/benches/crypto_benches/main.rs
+++ gotatun/benches/crypto_benches/main.rs
@@ -1,12 +1,9 @@
 use blake2s_benching::{bench_blake2s_hash, bench_blake2s_hmac, bench_blake2s_keyed};
-use chacha20poly1305_benching::bench_chacha20poly1305;
 
 mod blake2s_benching;
-mod chacha20poly1305_benching;
 
 criterion::criterion_group!(
     crypto_benches,
-    bench_chacha20poly1305,
     bench_blake2s_hash,
     bench_blake2s_hmac,
     bench_blake2s_keyed,
