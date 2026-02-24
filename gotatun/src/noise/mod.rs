ddd9f82ccb2e9a3ceee04c0ace5c4af306cc6bf8
David Lönnhager <david.l@mullvad.net>
2026-02-24 09:26:02 +0100

    fixup: use<'_>

--- gotatun/src/noise/mod.rs
+++ gotatun/src/noise/mod.rs
@@ -408,7 +408,7 @@ impl Tunn {
     }
 
     /// Return the receiving indices of all active sessions.
-    pub fn active_receiving_indices(&self) -> impl Iterator<Item = u32> + '_ {
+    pub fn active_receiving_indices(&self) -> impl Iterator<Item = u32> + use<'_> {
         self.sessions
             .iter()
             .filter_map(|s| s.as_ref().map(|s| s.receiving_index.value()))
