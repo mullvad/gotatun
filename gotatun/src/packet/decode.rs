0685a0f1a40f3c8ab1a3c6d45174b2b663c85fab
Joakim Hulthe <joakim.hulthe@mullvad.net>
2026-05-05 17:45:24 +0200

    Add license headers

--- gotatun/src/packet/decode.rs
+++ gotatun/src/packet/decode.rs
@@ -1,3 +1,14 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//   Copyright (c) Mullvad VPN AB. All rights reserved.
+//
+// SPDX-License-Identifier: MPL-2.0
+
 use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout, TryFromBytes};
 
 use crate::packet::Packet;
