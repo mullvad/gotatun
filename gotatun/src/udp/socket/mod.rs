fe67f0a438132fe27be7f32e23a3a741c0063143
Sebastian Holmin <sebastian.holmin@mullvad.net>
2026-08-06 15:45:40 +0200

    fixup

--- gotatun/src/udp/socket/mod.rs
+++ gotatun/src/udp/socket/mod.rs
@@ -422,7 +422,7 @@ mod tests {
                 .recv_many_from(&mut buf, &mut pool1, &mut out)
                 .await
                 .unwrap();
-            assert!(!out.is_empty() && out.len() < n);
+            assert!(!out.is_empty() && out.len() <= n);
             n -= out.len();
             for (p, src) in out {
                 assert_eq!(src, v6_addr);
